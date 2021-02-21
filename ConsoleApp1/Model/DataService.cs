142920f8b8df35e33dab2317590f706f5c6df68f
Alex M <xbestyan@hotmail.com>
2021-02-21 19:51:24 +0300

    add myproject

---
+++ ConsoleApp1/Model/DataService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+	public class DataService
+	{
+
+		private string FileName = "";
+		public DataService()
+		{
+			FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", $"gtrm_{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt");
+			CheckFile();
+		}
+		private void CheckFile()
+		{
+			if (File.Exists(FileName))
+			{
+				File.Delete(FileName);
+			}
+			else
+			{
+			}
+		}
+		public void WriteData(string ResString)
+		{
+			using (StreamWriter outputFile = new StreamWriter(FileName, true))
+			{
+				outputFile.WriteLine(ResString);
+			}
+
+			//if (File.Exists(AppPath))
+			//{
+
+			//}
+			//else
+			//{
+
+			//}
+			//using (FileStream fs = File.OpenWrite(AppPath))
+			//{
+			//	string output = ResString;
+			//	byte[] bytes = Encoding.Default.GetBytes(output);
+			//	fs.Write(bytes, 0, bytes.Length);
+			//}
+		}
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			DataService service = new DataService();
+			string result = "";
+			var collection = new Dictionary<int, IEnumerable<int>>();
+			for (int i = 0; i < 10000; i++)
+			{
+				collection.Add(i, Enumerable.Range(1, 10000));
+				IEnumerable<int> col = Enumerable.Range(1, 10000);
+				for (int j = 0; j < 65; j++)
+				{
+					
+					result = j == 0 ? $"{i};{j};" : $"{result}{j};";
+				}
+				result = $"{result}\r";
+				//service.WritekData(result);
+				//Console.WriteLine(result);
+			}
+			Console.ReadLine();
+			foreach (var item in collection)
+			{
+				
+				Console.WriteLine(item);
+			}
+		}
+
+
+	}
+}
